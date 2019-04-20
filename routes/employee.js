d1c0d40a011b83e4373dff113ef14c852b25546d
Chaitanya Vulchi <chaitanya@Chaitanyas-MacBook-Pro.local>
2019-04-20 19:58:37 +0530

    update

--- routes/employee.js
+++ routes/employee.js
@@ -56,7 +56,7 @@ router.get('/get-emp-details/:id', checkAuth, (req, res) => {
     })
 })
 
-router.post('/update-emp-details', (req, res) => {
+router.put('/update-emp-details', (req, res) => {
 
     let options = req.body
     console.log('options', options);
