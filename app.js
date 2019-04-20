9b0053d09b87b08295b3906a9af3581f6255397e
Chaitanya Vulchi <chaitanya@Chaitanyas-MacBook-Pro.local>
2019-04-20 13:59:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+console.log('hello world');
+
+const fs = require('fs') // Internal Module
+const os = require('os')
+const path = require("path");
+
+const appConfig = require('./config/appConfig')
+const userRoutes = require('./routes/users')
+
+
+// External Module
+const mongoose = require('mongoose')
+const express = require('express') 
+const bodyparser = require('body-parser')
+
+const app = express();
+
+
+// body parser for post methods
+ app.use(bodyparser.json())
+ app.use(bodyparser.urlencoded({extended: false}))
+
+ let modelsPath = './models'
+ 
+ fs.readdirSync(modelsPath).forEach(function (file) {
+    console.log(modelsPath + '/' + file)
+     if(~file.indexOf('.js')) require(modelsPath + '/' + file)
+    })
+    
+
+    app.use((req,res,next) => {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET,DELETE")
+
+        next();
+    })
+    
+   
+                        
+app.use('/user', userRoutes)
+
+app.listen(appConfig.port, () => {
+ 
+    let db = mongoose.connect(appConfig.db.url, ({ useNewUrlParser: true }))
+    console.log('Port is running in ' + appConfig.port)
+    console.log(appConfig.db.url);
+})
+
+
+mongoose.connection.on('error', function(err) {
+    if(err) {
+        console.log(err)
+    }
+})
+
+mongoose.connection.on('open', function(err) {
+    if(err) {
+        console.log(err)
+
+    } else {
+        console.log('connection successful')
+    }
+})
+
---
+++ routes/users.js
@@ -0,0 +1,163 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const router = express.Router()
+
+const model = require('../models/user')
+const UserModel = mongoose.model('User')
+
+router.get('/', (req, res) => {
+    res.send('this is router express')
+})
+
+
+// localhost:3000/user/signup
+router.post('/signup', (req, res) => {
+    console.log('req', req.body);
+
+
+    UserModel.findOne({
+        email: req.body.email
+    }).then(result => {
+
+        console.log('RESULT', result);
+
+        if (!result) {
+
+            bcryptjs.hash(req.body.password, 10).then(hash => {
+
+                // console.log('HASH', hash)
+                let userDetails = new UserModel({
+                    email: req.body.email,
+                    password: hash,
+                    isUser: req.body.isUser,
+                    name: req.body.name,
+                    mobileNumber: req.body.mobileNumber
+                })
+                console.log('userDetails', userDetails)
+
+                userDetails.save((err, result) => {
+                    if (err) {
+                        // res.send(err)
+                        res.status(500).json({
+                            message: 'Unable to create user',
+                            result: null,
+                            error: err
+                        })
+                    } else {
+                        //res.send(result)
+                        res.status(200).json({
+                            message: 'User Created Successfully',
+                            result: result,
+                            error: null
+                        })
+                    }
+                })
+            }).catch(err => {
+                res.status(500).json({
+                    message: 'Some error occured',
+                    result: null,
+                    err: err
+                })
+            })
+        } else {
+            // res.send('User already exist')
+            res.status(500).json({
+                message: 'User Already exist',
+                result: null,
+                err: null
+            })
+        }
+    }).catch(err => {
+        res.status(500).json({
+            message: 'Some error occured',
+            result: null,
+            err: err
+        })
+    })
+
+
+})
+
+
+// Email exist
+// password correct entered
+// generating auth token
+// Creating Login router
+
+router.post('/login', (req, res) => {
+    let userDetails
+    UserModel.findOne({
+        email: req.body.email
+    }).then(user => {
+        userDetails = user
+        if (!user) {
+
+            let resObj = {
+                message: 'No User Found',
+                status: 404,
+                error: null,
+                token: null,
+                result: null
+            }
+
+            return res.send(resObj)
+        }
+
+        bcryptjs.compare(req.body.password, user.password).then(isMatch => {
+            if (!isMatch) {
+
+
+                let resObj = {
+                    message: 'Password incorrect',
+                    status: 404,
+                    error: null,
+                    token: null,
+                    result: null
+                }
+
+                return res.send(resObj)
+            }
+
+
+            let token = jwt.sign({
+                    email: userDetails.email,
+                    userId: userDetails._id
+                },
+                'thisissecretkeyanditisverylong')
+
+            if (token) {
+
+                let resObj = {
+                    message: 'Login Successful',
+                    status: 200,
+                    error: null,
+                    token: token,
+                    result: userDetails
+                }
+                return res.send(resObj)
+            } else {
+
+                let resObj = {
+                    message: 'Unable to generate token',
+                    status: 404,
+                    error: null,
+                    token: null,
+                    result: null
+                }
+
+                return res.send(resObj)
+            }
+        })
+    }).catch(err => {
+        console.log(err)
+
+    })
+})
+
+
+
+
+module.exports = router;
